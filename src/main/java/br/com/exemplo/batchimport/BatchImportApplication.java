5b0c0006e767ac26ecb2cf35c9e3148767c5507a
Rafael Fonseca Hermes Azevedo <rafael.azevedo.dev@gmail.com>
2026-03-15 17:55:13 -0300

    commit inicial

---
+++ src/main/java/br/com/exemplo/batchimport/BatchImportApplication.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.batchimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchImportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchImportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/exemplo/batchimport/BatchImportApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.batchimport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchImportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
